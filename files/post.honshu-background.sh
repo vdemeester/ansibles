34c916686e0b6ab3f0e2d9f463fb24ee0b5be7ce
Vincent Demeester <vincent@sbr.pm>
2014-12-28 13:02:09 +0100

    Add background and xrandr files for honshu

---
+++ files/post.honshu-background.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Set a custom background to honshu computer
+feh --bg-fill ~/desktop/pictures/wallpapers/computers/honshu/wallpaper-1960999.jpg --bg-fill ~/desktop/pictures/wallpapers/computers/honshu/wallpaper-2312526.jpg
---
+++ files/pre.xrandr.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Run xrandr before starting the window manager
+
+# Specific to honshu for now
+xrandr --output VGA1 --auto --left-of eDP1
